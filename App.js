68044d6ce1889391361fe0562dd79f25f8f44554
Rohit Mondal <rahulsarker1495@gmail.com>
2023-12-07 20:28:42 +0530

    image section

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import image from '../my-app/components/image';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.h1}>Open up App.js to start working on your app!</Text>
+      <image></image>
+      <Image style={styles.img} source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJoDUR93fX22g3Eky6vyjQyyfugNVX9QI55EUrUETpRk61Ne35J4J5AxwzLt6tLTl_h_8&usqp=CAU"}}></Image>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +20,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 2,
+    borderColor: 'blue',
+    borderRadius: 10,
+    marginBottom: 20,
   },
+  h1: {
+    color: "red",
+    fontSize:50
+  },
+  img: {
+    width: 100,
+    height:100
+  }
+
 });
---
+++ components/image.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+
+export default function image() {
+  return (
+    <View>
+          <Text>image</Text>
+          
+    </View>
+  )
+}
+
